12369fe235053ddadd848eb33260136619826ce6
VirtualRohit29 <rohitkr290704@gmail.com>
2025-03-23 15:33:55 +0530

    maincontent added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import MainContent from './components/MainContent'
+
 
 const App = () => {
   return (
     <>
+   <div className='flex'>
    <Sidebar/>
+  <MainContent/>
+   </div>
     </>
   )
 }
